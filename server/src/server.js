3c111cc65b850c70f4b72e1995c23ce9ef78c2ef
abrar_398 <jucse29.398@gmail.com>
2025-03-20 02:03:24 +0600

    added env file and secrets.js

--- server/src/server.js
+++ server/src/server.js
@@ -1,6 +1,7 @@
 const app = require("./app");
+const { serverPort } = require("./secret");
 
 
-app.listen(3001, () => {
+app.listen(serverPort, () => {
     console.log('Server is running on port 3001');
 })
