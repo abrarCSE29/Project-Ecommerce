107f001dbc1831d1042acdaf88dbc4c484677cd8
abrar_398 <jucse29.398@gmail.com>
2025-03-26 19:49:08 +0600

    added search and pagination

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -5,7 +5,7 @@ const getUsers= async (req, res, next) => {
     try{
         const search = req.query.search || "";
         const page = Number(req.query.page) || 1;
-        const limit = Number(req.query.limit) || 5;
+        const limit = Number(req.query.limit) || 1;
         
         const searchRegExp = RegExp('.*'+ search + '.*','i');
         const filter = {
@@ -16,11 +16,24 @@ const getUsers= async (req, res, next) => {
                 {phone : {$regex : searchRegExp}},
             ]
         };
+        const options = {password : 0} 
         
-        const users = await User.find(filter);
+        const users = await User.find(filter, options)
+        .limit(limit)
+        .skip((page - 1) * limit);
+
+        const totalUsers = await User.countDocuments(filter);
+
+        if(!users) throw createError(404, 'No users found');
         res.status(200).json({
             message: "User list is returned from models",
-            users : users
+            users : users,
+            pagination:{
+                totalPages: Math.ceil(totalUsers /limit),
+                currentPage : page,
+                previousPage : page-1>0? page-1 : null,
+                nextPage : page+1<=Math.ceil(totalUsers / limit)? page+1 : null,
+            }
         });
     }
     catch(err){
