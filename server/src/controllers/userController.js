6c3a273a1710d8599717223a5c96cf3910973a56
abrar_398 <jucse29.398@gmail.com>
2025-03-26 19:36:37 +0600

    added filter

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -3,7 +3,21 @@ const User = require('../models/userModel');
 
 const getUsers= async (req, res, next) => {
     try{
-        const users = await User.find();
+        const search = req.query.search || "";
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 5;
+        
+        const searchRegExp = RegExp('.*'+ search + '.*','i');
+        const filter = {
+            isAdmin: {$ne: true},
+            $or :[
+                {name : {$regex : searchRegExp}},
+                {email : {$regex : searchRegExp}},
+                {phone : {$regex : searchRegExp}},
+            ]
+        };
+        
+        const users = await User.find(filter);
         res.status(200).json({
             message: "User list is returned from models",
             users : users
