72f6ea31314498f1ebe79f57fb172ffeb45f7d5e
abrar_398 <jucse29.398@gmail.com>
2025-03-26 19:21:48 +0600

    added api for getting all users

--- server/src/controllers/userController.js
+++ server/src/controllers/userController.js
@@ -1,9 +1,9 @@
 const createError = require('http-errors');
+const User = require('../models/userModel');
 
-
-
-const getUsers=((req, res, next) => {
+const getUsers= async (req, res, next) => {
     try{
+        const users = await User.find();
         res.status(200).json({
             message: "User list is returned from models",
             users : users
@@ -12,7 +12,7 @@ const getUsers=((req, res, next) => {
     catch(err){
         next(err);
     }
-}); 
+}; 
 
 
 module.exports = { getUsers };
